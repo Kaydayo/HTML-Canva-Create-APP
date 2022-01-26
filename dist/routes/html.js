a63fe106bacff790fe601d18e0c62685c2a99018
kaydayo <olukayodedayo100@gmail.com>
2022-01-26 05:49:19 +0100

    added dist/app

---
+++ dist/routes/html.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var htmlController_1 = require("../controllers/htmlController");
+var router = express_1.default.Router();
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send(200).json({ message: 'you are here' });
+});
+router.post('/create/:html', htmlController_1.createHtml);
+router.get('/get/webview/:id', htmlController_1.getHtml);
+exports.default = router;
